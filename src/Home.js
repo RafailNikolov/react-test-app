ef308844bb53aed258c054b632015046f4a31bee
RafailNikolov <nikolov_rafail@yahoo.com>
2021-09-27 18:16:12 +0300

    added some stuff

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Row, Col, Container, Image } from 'react-bootstrap';
+import homeImage from "./assets/technology.jpg"
 
 export const Home = ({lang}) => {
     return (
-        <div>
-            <h1>{lang.title}</h1>
-        </div>
+        <React.Fragment>
+            <Row style={{"text-align" : "center"}} >
+                <h1>{lang.title}</h1>
+            </Row>
+            
+            <Row>
+                <Col lg={6}>
+                    <Image src={homeImage} fluid></Image>
+                </Col>
+                <Col lg={6} style={{"margin": "auto"}}>
+                    {lang.text}
+                </Col>
+            </Row>
+           
+        </React.Fragment>
     )
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
-import { DropdownButton, Dropdown } from 'react-bootstrap';
+import { DropdownButton, Dropdown, Image } from 'react-bootstrap';
 import FlagIcon from '../FlagIcon';
 import { useState } from 'react';
+import logo from '../assets/heptagon-logo.png';
+import "./Navigation.css";
 
 export const NavigationBar = ({lang, changeLang}) => {
 
@@ -15,26 +17,26 @@ export const NavigationBar = ({lang, changeLang}) => {
     }
 
     return (
-        <Navbar expand='lg'>
-            <Navbar.Brand href='/'>{lang.navTitle}</Navbar.Brand>
+        <Navbar expand='lg' className="custom-navbar-class">
+            <Image className="custom-image-class" src={logo} rounded/>
+            <Navbar.Brand href='/' className="custom-navbar-brand-class">{lang.navTitle}</Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
             <Navbar.Collapse id='basic-navbar-nav' className="justify-content-end">
                 <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href='/'>{lang.btnHome}</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href='/contacts'>{lang.btnContacts}</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href='/about'>{lang.btnAbout}</Nav.Link></Nav.Item>
+                    <Nav.Item className="custom-navitem-class"><Nav.Link className="custom-navlink-class" href='/'>{lang.btnHome}</Nav.Link></Nav.Item>
+                    <Nav.Item className="custom-navitem-class"><Nav.Link className="custom-navlink-class" href='/contacts'>{lang.btnContacts}</Nav.Link></Nav.Item>
+                    <Nav.Item className="custom-navitem-class"><Nav.Link className="custom-navlink-class" href='/about'>{lang.btnAbout}</Nav.Link></Nav.Item>
+                    <Nav.Item className="custom-navitem-class">
+                        <DropdownButton id="dropdown-basic-button" title={<FlagIcon code={countryFlag} size={"2x"} />} onSelect={handleSelect}>
+                            <Dropdown.Item eventKey='bg' href="#" onClick={ (e) => { e.preventDefault(); changeLang('bg')}}>
+                                <FlagIcon code={"bg"} size={"2x"} /> Български
+                            </Dropdown.Item>
+                            <Dropdown.Item eventKey='gb' href="#" onClick={ (e) => { e.preventDefault(); changeLang('gb')}}>
+                                <FlagIcon code={"gb"} size={"2x"} /> English
+                            </Dropdown.Item>
+                        </DropdownButton>
+                    </Nav.Item>
                 </Nav>
-
-                <DropdownButton id="dropdown-basic-button" title={<FlagIcon code={countryFlag} size={"1x"} />} style={{"width" : "110px"}} 
-                onSelect={handleSelect} >
-                    <Dropdown.Item eventKey='bg' href="#" onClick={ (e) => { e.preventDefault(); changeLang('bg')}}>
-                        <FlagIcon code={"bg"} size={"1x"} /> Български
-                    </Dropdown.Item>
-                    <Dropdown.Item eventKey='gb' href="#" onClick={ (e) => { e.preventDefault(); changeLang('gb')}}>
-                        <FlagIcon code={"gb"} size={"1x"} /> English
-                    </Dropdown.Item>
-                </DropdownButton>
-
             </Navbar.Collapse>
         </Navbar>
     )
